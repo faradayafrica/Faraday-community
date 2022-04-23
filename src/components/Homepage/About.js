fe6ed6e046d4dbf1624892fa0510bd8a3c157f95
Faraday <90288585+faradayadmin@users.noreply.github.com>
2022-04-23 21:46:59 +0100

    Updated about

--- src/components/Homepage/About.js
+++ src/components/Homepage/About.js
@@ -6,9 +6,9 @@ function About() {
       <h2 className='text-center text-xl md:text-2xl font-bold'>About</h2>
 
       <p className='mt-4 text-center max-w-[40rem] mx-auto'>
-        Faraday is a community driven ed-tech solution targeted towards tertiary
-        ínstitution students to help improve the learning experience. We're
-        working tirelessly to build a community where scholars can share what
+        Faraday is a Community driven Ed-tech solution improving the learning experience for Students in tertiary
+        ínstitutions. We're 
+        working tirelessly to build a community where everyone can share what
         they know with others who need help.
       </p>
     </section>
