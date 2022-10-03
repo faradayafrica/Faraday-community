4bcb0859f4e498704773b11856e12f7312c7191f
Martin Kostadinov <martin.kostadinov@cognitrek.com>
2022-10-03 16:38:24 +0200

    add faraday about us link and open it in new tab when clicked

--- src/util.js
+++ src/util.js
@@ -3,3 +3,5 @@ import Airtable from "airtable";
 export const base = new Airtable({
   apiKey: process.env.REACT_APP_API_KEY,
 }).base(process.env.REACT_APP_BASE);
+
+export const FARADAY_ABOUT_URL = "https://about.faraday.africa/"
