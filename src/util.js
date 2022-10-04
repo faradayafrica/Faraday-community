7748a12989adba6697c3df26e5e03e53c232da41
Francis <francisalexander000@gmail.com>
2022-10-04 14:10:08 +0100

    Added env variables

--- src/util.js
+++ src/util.js
@@ -1,12 +1,7 @@
 import Airtable from "airtable";
 
-// export const base = new Airtable({
-//   apiKey: process.env.REACT_APP_API_KEY,
-// }).base(process.env.REACT_APP_BASE);
-
 export const base = new Airtable({
-  apiKey: 'appWohC2Yg2fWL19o',
-}).base('keyPQpdNb1ZTL6bzY');
-
+  apiKey: process.env.REACT_APP_API_KEY,
+}).base(process.env.REACT_APP_BASE);
 
 export const FARADAY_ABOUT_URL = "https://about.faraday.africa/"
